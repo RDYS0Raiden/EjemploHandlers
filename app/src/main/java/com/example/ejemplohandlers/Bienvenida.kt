8e92ac4687a1d0e561037b57ebd88379c60a0e02
RDYS0yamato <yamatotshiro@gmail.com>
2022-09-22 10:21:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ejemplohandlers/Bienvenida.kt
@@ -0,0 +1,11 @@
+package com.example.ejemplohandlers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenida)
+    }
+}
---
+++ app/src/main/java/com/example/ejemplohandlers/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ejemplohandlers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ejemplohandlers.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+}
