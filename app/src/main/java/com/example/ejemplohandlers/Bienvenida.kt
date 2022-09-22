2a739aad8403276be52950476015f2fc7694df79
RDYS0yamato <yamatotshiro@gmail.com>
2022-09-22 10:38:54 -0400

    .

--- app/src/main/java/com/example/ejemplohandlers/Bienvenida.kt
+++ app/src/main/java/com/example/ejemplohandlers/Bienvenida.kt
@@ -2,10 +2,27 @@ package com.example.ejemplohandlers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejemplohandlers.databinding.ActivityBienvenidaBinding
 
 class Bienvenida : AppCompatActivity() {
+    //en el binding en el private lateinit se debe referencia a la vista que se este haciendo
+    private lateinit var binding: ActivityBienvenidaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bienvenida)
+        binding= ActivityBienvenidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       //Si esta Pantalla es lanzada de una intencion
+        //en una variable llamada intent ya se encontrara toda
+        //la configuracion enviada y por tanto las variables en extras
+        intent.getStringExtra("texto")
+        intent.getIntExtra("valor",0)
+        val respuesta="""
+            el texto es:${intent.getStringExtra("texto")}
+            su valor es:${intent.getIntExtra("valor",0)}
+        """.trimIndent()
+        binding.txtResultado.text=respuesta
+
     }
+
 }
--- app/src/main/java/com/example/ejemplohandlers/MainActivity.kt
+++ app/src/main/java/com/example/ejemplohandlers/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ejemplohandlers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejemplohandlers.databinding.ActivityMainBinding
@@ -13,8 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.btnIniciar.setOnClickListener {
+            pasarPantalla()
+        }
 
     }
+   private fun pasarPantalla(){
+        val mensaje="Estudiante"
+       val numero =10
+       //configurar un intent para generar la intencion de querer pasar
+       //de una pantalla A a una Pantalla B enviamos datos
+       //primitivos.....
+       //Pasar datos se usa archivo tempotral llamado Extras..
+       //Extras:formato de cada registro es:llave -valor
+       val intent=Intent(this, Bienvenida ::class.java)
+           intent.apply {
+               putExtra("texto",mensaje)
+               putExtra("valor",numero)
+           }
 
+       startActivity(intent)
+    }
 }
